d4ad9d2180e8cb8df183e896b04c221c34b90b62
yrR1cHu <114379744+yrR1cHu@users.noreply.github.com>
2025-05-10 07:16:04 +0800

    调整了 settings.json 配置文件

---
+++ config.py
@@ -0,0 +1,15 @@
+# General settings for launching the program
+import platform
+
+host: str = "localhost"
+port: int = 50001
+recv_size: int = 10240
+name: str = "SLAM Box"
+version: str = "0.8.1"
+system: str = platform.system()
+gui: str = "Node-based UI"
+description: str = "Computer Vision Node Graph Editor"
+date: str = "(Thu Apr 17 12:14:42 AM EEST 2025)"
+nodegraphqt: str = "./NodeGraphQt/"
+css_style: str = "QLabel {background-color: #363636; color: white; font-size: 11pt;}"
+g2opy_path: str = "/home/cds/github/g2o-pymem/build/lib"
